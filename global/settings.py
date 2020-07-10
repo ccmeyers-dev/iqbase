84ee400c058a1023aaf2cfb5bced4ac828da8ec9
ccmeyers-dev <charlotteccmeyers@outlook.com>
2020-07-10 01:19:50 +0100

    commit local files before updating static files and removing images

--- global/settings.py
+++ global/settings.py
@@ -8,7 +8,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 from distutils.util import strtobool
-
+from dotenv import load_dotenv
+load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
