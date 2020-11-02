2e54889db0c3e1ffe3939a419e32a8d8ed929e75
ccmeyers-dev <charlotteccmeyers@outlook.com>
2020-11-02 23:22:13 +0100

    initial uplift

--- light/broker_forms.py
+++ light/broker_forms.py
@@ -1,15 +1,29 @@
 from django import forms
-from broker.models import Trade, Deposit, Customer, Wallet
+from broker.models import Trade, Deposit, Customer, Wallet, Identity
+
+
+class IdentityForm(forms.ModelForm):
+    class Meta:
+        model = Identity
+        fields = '__all__'
+        widgets = {'user': forms.HiddenInput()}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['address'].widget.attrs.update({'class': 'form-control'})
+
 
 class WalletForm(forms.ModelForm):
     class Meta:
         model = Wallet
         fields = '__all__'
-        widgets = {'coin': forms.HiddenInput(), 'code': forms.HiddenInput(), 'hue': forms.HiddenInput()}
+        widgets = {'coin': forms.HiddenInput(), 'code': forms.HiddenInput(),
+                   'hue': forms.HiddenInput()}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['address'].widget.attrs.update({'class': 'form-control'})
+
 
 class depocorrectform(forms.ModelForm):
     class Meta:
@@ -19,12 +33,14 @@ class depocorrectform(forms.ModelForm):
             'customer': forms.HiddenInput(),
             'wallet': forms.HiddenInput(),
             'date_created': forms.HiddenInput()
-            }
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['amount'].widget.attrs.update({'class': 'form-control'})
-        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update(
+            {'class': 'form-control'})
+
 
 class quickfundform(forms.ModelForm):
     class Meta:
@@ -35,11 +51,12 @@ class quickfundform(forms.ModelForm):
             'amount': forms.HiddenInput(),
             'wallet': forms.HiddenInput(),
             'duration': forms.HiddenInput(),
-            }
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['profit'].widget.attrs.update({'class': 'form-control'})
+
 
 class fundform(forms.ModelForm):
     class Meta:
@@ -54,25 +71,31 @@ class fundform(forms.ModelForm):
         self.fields['profit'].widget.attrs.update({'class': 'form-control'})
         self.fields['duration'].widget.attrs.update({'class': 'form-control'})
 
+
 class coinform(forms.ModelForm):
     class Meta:
         model = Trade
         fields = '__all__'
-        widgets = {'customer': forms.HiddenInput(), 'profit': forms.HiddenInput()}
+        widgets = {'customer': forms.HiddenInput(
+        ), 'profit': forms.HiddenInput()}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['wallet'].widget.attrs.update({'class': 'form-control', 'hidden':'hidden'})
+        self.fields['wallet'].widget.attrs.update(
+            {'class': 'form-control', 'hidden': 'hidden'})
         self.fields['amount'].widget.attrs.update({'class': 'form-control'})
         self.fields['duration'].widget.attrs.update({'class': 'form-control'})
+
 
 class coindepoform(forms.ModelForm):
     class Meta:
         model = Deposit
         fields = '__all__'
-        widgets = {'customer': forms.HiddenInput(), 'wallet': forms.HiddenInput()}
+        widgets = {'customer': forms.HiddenInput(
+        ), 'wallet': forms.HiddenInput()}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update(
+            {'class': 'form-control'})
         self.fields['amount'].widget.attrs.update({'class': 'form-control'})
--- light/urls.py
+++ light/urls.py
@@ -3,13 +3,13 @@ from . import views
 from . import auth_views as authviews
 
 urlpatterns = [
-    #home
+    # home
     path('about/', views.about, name='about'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('term-condition/', views.term_condition, name='term_condition'),
 
 
-    #user
+    # user
     path('', views.home, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('customer-data/', views.customer_data, name='customer_data'),
@@ -19,49 +19,70 @@ urlpatterns = [
     path('account/', views.account, name='account'),
     path('profile/', views.userprofile, name='userprofile'),
     path('settings&action=name/', views.settings_name, name='settings_name'),
-    path('settings&action=profile/', views.settings_profile, name='settings_profile'),
-    path('settings&action=contact/', views.settings_contact, name='settings_contact'),
+    path('settings&action=profile/',
+         views.settings_profile, name='settings_profile'),
+    path('settings&action=contact/',
+         views.settings_contact, name='settings_contact'),
+    path('settings&action=identity/',
+         views.settings_identity, name='settings_identity'),
 
 
-    #admin
+    # admin
     path('administrator/', views.administrator, name='administrator'),
     path('user&target=<str:pk>/', views.user, name='user'),
     path('receipt/', views.receipt, name='receipt'),
     path('delete-user&target=<str:pk>/', views.del_user, name='del_user'),
-    path('toggle-admin&target=<str:pk>/', views.toggle_admin, name='toggle_admin'),
-    path('deactivate-user&target=<str:pk>/', views.deactivate, name='deactivate'),
+    path('toggle-admin&target=<str:pk>/',
+         views.toggle_admin, name='toggle_admin'),
+    path('deactivate-user&target=<str:pk>/',
+         views.deactivate, name='deactivate'),
     path('activate-user&target=<str:pk>/', views.activate, name='activate'),
-    #edit
-    path('update-trade&target=<str:pk>/', views.update_trade, name='update_trade'),
-    path('delete-trade&target=<str:pk>/', views.delete_trade, name='delete_trade'),
-    path('update-deposit&target=<str:pk>/', views.update_deposit, name='update_deposit'),
-    path('delete-deposit&target=<str:pk>/', views.delete_deposit, name='delete_deposit'),
-    #deposit
-    path('deposit&coin=bitcoin&target=<str:pk>/', views.bitcoindepo, name='bitcoindepo'),
-    path('deposit&coin=ethereum&target=<str:pk>/', views.ethereumdepo, name='ethereumdepo'),
-    path('deposit&coin=litecoin&target=<str:pk>/', views.litecoindepo, name='litecoindepo'),
-    #trade
+    # reject card details
+    path('reject_card/<str:pk>/', views.reject_card, name='reject_card'),
+    # edit
+    path('update-trade&target=<str:pk>/',
+         views.update_trade, name='update_trade'),
+    path('delete-trade&target=<str:pk>/',
+         views.delete_trade, name='delete_trade'),
+    path('update-deposit&target=<str:pk>/',
+         views.update_deposit, name='update_deposit'),
+    path('delete-deposit&target=<str:pk>/',
+         views.delete_deposit, name='delete_deposit'),
+    # deposit
+    path('deposit&coin=bitcoin&target=<str:pk>/',
+         views.bitcoindepo, name='bitcoindepo'),
+    path('deposit&coin=ethereum&target=<str:pk>/',
+         views.ethereumdepo, name='ethereumdepo'),
+    path('deposit&coin=litecoin&target=<str:pk>/',
+         views.litecoindepo, name='litecoindepo'),
+    # trade
     path('trade&coin=bitcoin&target=<str:pk>/', views.bitcoin, name='bitcoin'),
-    path('trade&coin=ethereum&target=<str:pk>/', views.ethereum, name='ethereum'),
-    path('trade&coin=litecoin&target=<str:pk>/', views.litecoin, name='litecoin'),
-    #withdrawal
-    path('withdrawal&coin=bitcoin&target=<str:pk>/', views.bitcoinwith, name='bitcoinwith'),
-    path('withdrawal&coin=ethereum&target=<str:pk>/', views.ethereumwith, name='ethereumwith'),
-    path('withdrawal&coin=litecoin&target=<str:pk>/', views.litecoinwith, name='litecoinwith'),
-    
+    path('trade&coin=ethereum&target=<str:pk>/',
+         views.ethereum, name='ethereum'),
+    path('trade&coin=litecoin&target=<str:pk>/',
+         views.litecoin, name='litecoin'),
+    # withdrawal
+    path('withdrawal&coin=bitcoin&target=<str:pk>/',
+         views.bitcoinwith, name='bitcoinwith'),
+    path('withdrawal&coin=ethereum&target=<str:pk>/',
+         views.ethereumwith, name='ethereumwith'),
+    path('withdrawal&coin=litecoin&target=<str:pk>/',
+         views.litecoinwith, name='litecoinwith'),
 
-    #semi auth urls
-    path('change-password&account=<str:cust>/', views.settings_password, name='change_password'),
+
+    # semi auth urls
+    path('change-password&account=<str:cust>/',
+         views.settings_password, name='change_password'),
     path('reset-password/', views.reset_password, name='reset_password'),
     path('action-block/', views.action_block, name='action_block'),
 
 
-    #auth urls
+    # auth urls
     path('register/', authviews.register_view, name='register'),
     path('referral/', authviews.referral, name='referral'),
     path('register/ref=<str:ref>', authviews.register_view_ref, name='register_ref'),
     path('profile-authentication/', authviews.profile, name='profile'),
     path('login/', authviews.login_view, name="login"),
     path('logout/', authviews.logout_view, name="logout"),
-    re_path('$', authviews.login_view),
+    # re_path('$', authviews.login_view),
 ]
