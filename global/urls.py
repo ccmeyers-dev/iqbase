1bc49bf0d7dfb6d19762744502497cbca15c3f5e
ccmeyers-dev <charlotteccmeyers@outlook.com>
2021-01-18 14:25:33 +0100

    add livechat

--- global/urls.py
+++ global/urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('light.urls')),
-    # path('', include('dark.urls')),
+    # path('', include('light.urls')),
+    path('', include('dark.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
